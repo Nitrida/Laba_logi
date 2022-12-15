ae9e71019654a4c7fb9e689d3d1ad1bb8d7bfa9c
Nitrida <71544383+Nitrida@users.noreply.github.com>
2022-12-15 15:23:25 +0300

    Create log.py

---
+++ log.py
@@ -0,0 +1,48 @@
+import gzip
+import io
+import os, re
+
+config = {
+    "LOG_DIR": "./log"
+}
+
+
+def main():
+    log_names = sorted(os.listdir(config.get('LOG_DIR')))
+    if not log_names: return print(f'Logs not found')
+    log_name = log_names[-1]
+    log_path = f'{config.get("LOG_DIR")}/{log_name}'
+    pattern = re.compile(r'\"[A-Z]+ (\S+) .* (\d+\.\d+)\n')
+    data = {}
+    total_count = 0
+
+    def read_log(iterable_entity):
+        nonlocal total_count
+        for line in iterable_entity:
+            result = pattern.findall(line)
+            total_count += 1
+            if not len(result): continue
+            url, time = result[0]
+            if url not in data: data[url] = []
+            data[url].append(float(time))
+
+    if os.path.splitext(log_path)[1] == '.gz':
+        with gzip.open(log_path, 'r') as archive:
+            with io.TextIOWrapper(archive, encoding='utf-8') as decoder:
+                read_log(decoder)
+    else:
+        with open(log_path, 'r') as file:
+            read_log(file)
+
+    for key in data:
+        count = len(data[key])
+        print(f'URL: {key}'
+              f'\tcount: {count}'
+              f'\n\tcount_perc: {count / total_count * 100}'
+              f'\n\ttime_avg: {sum(data[key]) / count}'
+              f'\n\ttime_max: {max(data[key])}'
+              f'\n\ttime_med: {data[key][count // 2] if not data[key].lenght % 2 else (data[key][count // 2] + data[key][count // 2 + 1]) / 2}')
+
+
+if __name__ == "__main__":
+    main()
